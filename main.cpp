632afe779690654064c504817d7397c923bdcf5e
Jason <goodcheeseburgers@gmail.com>
2020-04-24 23:56:37 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include "SFML/Graphics.hpp"
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event{};
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+
+            window.clear();
+            window.display();
+        }
+    }
+
+    // End Application
+    return 0;
+}
